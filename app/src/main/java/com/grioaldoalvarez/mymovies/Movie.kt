48c31157e892fc1088e11f31f5a954aff3859bff
Grioaldo Iraidez Alvarez Romero <gros83@hotmail.com>
2023-05-30 20:44:35 -0600

    Inflando una vista para el recicleView sin usar viewbinding

---
+++ app/src/main/java/com/grioaldoalvarez/mymovies/Movie.kt
@@ -0,0 +1,3 @@
+package com.grioaldoalvarez.mymovies
+
+data class Movie(val title: String, val cover: String)
--- app/src/main/java/com/grioaldoalvarez/mymovies/MoviesAdapter.kt
+++ app/src/main/java/com/grioaldoalvarez/mymovies/MoviesAdapter.kt
@@ -1,18 +1,21 @@
 package com.grioaldoalvarez.mymovies
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class MoviesAdapter: RecyclerView.Adapter<MoviesAdapter.ViewHolder>() {
+class MoviesAdapter(private val movies: List<Movie>): RecyclerView.Adapter<MoviesAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.view_movie_item, parent, false)
+
+        return ViewHolder(view)
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = movies.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         TODO("Not yet implemented")
