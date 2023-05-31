390a6e71f661219908f4b32f1922db764d61670d
Grioaldo Iraidez Alvarez Romero <gros83@hotmail.com>
2023-05-30 21:38:21 -0600

    RecyclerView ejecutandose ya correctamente

--- app/src/main/java/com/grioaldoalvarez/mymovies/MainActivity.kt
+++ app/src/main/java/com/grioaldoalvarez/mymovies/MainActivity.kt
@@ -21,7 +21,16 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         val binding  = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.recycler.adapter = MoviesAdapter()
+        binding.recycler.adapter = MoviesAdapter(
+            listOf(
+                Movie("Title 1","url 1"),
+                Movie("Title 2","url 2"),
+                Movie("Title 3","url 3"),
+                Movie("Title 4","url 4"),
+                Movie("Title 5","url 5"),
+                Movie("Title 6","url 6")
+            )
+        )
     }
 
     override fun onDestroy() {
--- app/src/main/java/com/grioaldoalvarez/mymovies/MoviesAdapter.kt
+++ app/src/main/java/com/grioaldoalvarez/mymovies/MoviesAdapter.kt
@@ -4,22 +4,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.grioaldoalvarez.mymovies.databinding.ViewMovieItemBinding
 
 class MoviesAdapter(private val movies: List<Movie>): RecyclerView.Adapter<MoviesAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater
-            .from(parent.context)
-            .inflate(R.layout.view_movie_item, parent, false)
+        val binding = ViewMovieItemBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
 
-        return ViewHolder(view)
+        return ViewHolder(binding)
     }
 
     override fun getItemCount(): Int = movies.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bind(movies[position])
     }
 
-    class ViewHolder(view: View): RecyclerView.ViewHolder(view)
+    class ViewHolder(private val binding: ViewMovieItemBinding): RecyclerView.ViewHolder(binding.root){
+        fun bind(movie: Movie){
+            binding.title.text = movie.title
+        }
+    }
 }
